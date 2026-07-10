83fac9d3109161c2446ca90d1760c34760dc9ac3
chadfurman <chadfurman@users.noreply.github.com>
2026-07-10 07:54:14 -0400

    Revert "Merge pull request #3 from buildaloud/cloudflare/workers-autoconfig"
    This reverts commit 8fabcd52e7cf3cb7519911a75d8f5599828794ae, reversing
    changes made to 83fecc0227e6deca67b1eed1bc9b32ea3fe43f48.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://buildaloud.ai',
   integrations: [sitemap()],
-  adapter: cloudflare()
 });
